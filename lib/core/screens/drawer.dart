4d2d06dec3dd2a042f2d0bb509def74c5c3c11c3
Muhammad-Rehab <mohammedeid0303@gmail.com>
2023-12-12 03:40:35 +0200

    add contact us action

--- lib/core/screens/drawer.dart
+++ lib/core/screens/drawer.dart
@@ -1,12 +1,12 @@
 import 'package:dalily/core/helper/admin_helper.dart';
 import 'package:dalily/core/screens/admin_page.dart';
-import 'package:dalily/core/util/app_strings.dart';
 import 'package:dalily/core/util/styles.dart';
 import 'package:dalily/features/authentication/presentation/widgets/logout_record.dart';
-import 'package:dalily/features/categories/presentation/widgets/category_drawer_button.dart';
 import 'package:dalily/features/language/presentation/widget/language_record.dart';
 import 'package:dalily/features/theme/presentation/widgets/theme_record.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class AppDrawer extends StatelessWidget {
   final bool isArabic;
@@ -47,6 +47,17 @@ class AppDrawer extends StatelessWidget {
           Builder(builder: (context) {
             return LogInOrOutRecord();
           }),
+          TextButton(
+            onPressed: () async{
+              await launchUrlString("https://wa.me/+201145135267",
+              mode: LaunchMode.externalNonBrowserApplication,
+              );
+            },
+            child: Text(
+              AppLocalizations.of(context)!.contact_us,
+              style: bodySmall(context).copyWith(fontWeight: FontWeight.bold),
+            ),
+          )
         ],
       ),
     );
